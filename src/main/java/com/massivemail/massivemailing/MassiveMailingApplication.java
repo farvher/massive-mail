fafb4dd8d0322d1b905ff97da4f6fa13fd43990b
farvher <fesanmiguel@misena.edu.co>
2017-10-17 11:45:13 -0500

    Firts commit

---
+++ src/main/java/com/massivemail/massivemailing/MassiveMailingApplication.java
@@ -0,0 +1,12 @@
+package com.massivemail.massivemailing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassiveMailingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassiveMailingApplication.class, args);
+	}
+}
